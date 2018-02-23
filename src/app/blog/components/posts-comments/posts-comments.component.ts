97fee0c1b01514e73747e881c4057601d7c1bf29
Kortyka Thomas <tkortyka@alegri.eu>
2018-02-23 15:46:29 +0100

    Comments only Visible when logged

--- src/app/blog/components/posts-comments/posts-comments.component.ts
+++ src/app/blog/components/posts-comments/posts-comments.component.ts
@@ -9,6 +9,7 @@ import { DomSanitizer, SafeResourceUrl, SafeUrl, SafeHtml, SafeStyle } from '@an
 export class PostsCommentsComponent implements OnInit {
 
   dropdownOpen = false;
+  logged = false;
 
   @Input() comments: any[];
   @Input() loading: boolean;
