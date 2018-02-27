24e6c679174584e674bb51f7149c6d479fa18995
Kortyka Thomas <tkortyka@alegri.eu>
2018-02-27 14:03:52 +0100

    AOT Compiler

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from '@angular/platform-browser';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
@@ -9,4 +10,5 @@ if (environment.production) {
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+// platformBrowser().bootstrapModuleFactory(AppModule)
   .catch(err => console.log(err));
