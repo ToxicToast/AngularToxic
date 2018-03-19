838f15691878a2e619d2a23c30216804a9ef3837
Tom <info@toxictoast.de>
2018-03-19 17:40:35 +0100

    bugfix

--- deploy.sh
+++ deploy.sh
@@ -22,8 +22,7 @@ else
 		if [ "$f" != ".travis.yml" ] && [ "$f" != "deploy.sh" ] && [ "$f" != "test.js" ] && [ "$f" != "package.json" ]
 		then
 	 		echo "Uploading $f"
-	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS ftp://ftp.strato.de/$f
+	 		curl --ftp-create-dirs -T $f -u $FTP_USER:$FTP_PASS "ftp://ftp.strato.de/$f"
 		fi
 	done
 fi
-read
